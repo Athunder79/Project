229f6e131fd70a6d65947d7d9b290cd96924cbf9
Athunder79 <athunder@me.com>
2023-07-26 18:45:09 +0100

    finish JS on table

--- Module 2/script1.js
+++ Module 2/script1.js
@@ -1,10 +1,9 @@
-window.addEventListener('load', function () {
+addEventListener('load', function () {
     document.getElementById("start").addEventListener("click", showholength);
     document.getElementById("hole1").addEventListener("click", submitDetails);
     document.getElementById("shot").addEventListener("click", addShot);
     document.getElementById("finishhole").addEventListener("click", nexthole);
 })
-
 score = 0
 const dist = [];
 const par = [];
@@ -23,24 +22,41 @@ function showholength(){
     document.getElementById("holenum").className= "center";
 }
 
-// push hole detail to html
+// push hole detials to arrays and to html
 function submitDetails(){
     document.getElementById("holeLength").className = "hide";
     document.getElementById("form").className ="center";
     dist.push(+document.getElementById("HoleLnth").value);
     par.push(+document.getElementById("HolePar").value); 
-    push();
+    pushHoleDetails();
 }
 
-function push(){
+function pushHoleDetails(){
     let i = dist.length;
     document.getElementById(`par${i}`).innerHTML = par[i-1];
     document.getElementById(`dis${i}`).innerHTML = dist[i-1];
-    document.getElementById("totalFrontDist").innerHTML = totalFrontCalc();
-    document.getElementById("totalFrontPar").innerHTML = par.reduce((x, y) => x + y);
-    };
 
-function totalFrontCalc(){
+    if (dist.length <= 9){
+        document.getElementById("totalFrontDist").innerHTML = totalDist();
+        document.getElementById("totalFrontPar").innerHTML = totalPar();
+        document.getElementById("totalRndDis").innerHTML = totalDist();
+        document.getElementById("totalRndPar").innerHTML = totalPar();
+    }
+
+    else{
+
+        document.getElementById("totalBkDis").innerHTML = dist.slice(8, dist.length-1).reduce((x, y) => x + y);
+        document.getElementById("totalRndDis").innerHTML = totalDist();
+        document.getElementById("totalBkPar").innerHTML = par.slice(8, par.length-1).reduce((x, y) => x + y);
+        document.getElementById("totalRndPar").innerHTML = totalPar();
+    };
+       
+}
+function totalPar(){
+    return par.reduce((x, y) => x + y);     
+    }
+
+function totalDist(){
     return dist.reduce((x, y) => x + y);
     };
 
@@ -48,7 +64,10 @@ function totalFrontCalc(){
     function addShot(){
         let i = dist.length;
         scoreAdd1();
-        clubDist.push([document.getElementById("shotdist").value,document.getElementById("club").value]);
+        clubDist.push([document.getElementById("holenum").innerText, 
+                       document.getElementById("club").value, 
+                       +document.getElementById("shotdist").value]);
+                       
         document.getElementById(`scor${i}`).innerHTML = score;
         document.getElementById("shotinfo").innerHTML = 'Shot ' + (score + 1);
     };
@@ -63,29 +82,36 @@ function scoreAdd1(){
 function nexthole(){
     shots.push(score);
     let i = shots.length;
+    
     document.getElementById(`scor${i}`).innerHTML = shots[i-1];
-    document.getElementById(`ou${i}`).innerHTML =parseInt(document.getElementById(`ou${i-1}`).innerHTML) + parseInt(shots[i-1] - par[i-1]);
-        
-    if (dist.length <= 9){
-        document.getElementById("totalFrontScor").innerHTML = shots.reduce((x, y) => x + y)
-        document.getElementById("totalRndScor").innerHTML = shots.reduce((x, y) => x + y)}
-    else{
-        document.getElementById("totalBkScor").innerHTML = shots.slice(8, shots.length-1).reduce((x, y) => x + y);
-        document.getElementById("totalRndScor").innerHTML = shots.reduce((x, y) => x + y);
-    };
-    highLow();
+    document.getElementById(`ou${i}`).innerHTML = currentOverUnder();
+    document.getElementById("totalFrontOU").innerHTML = currentOverUnder();
+    document.getElementById("totalRndOu").innerHTML = currentOverUnder();
+        // checks if course position is front 9, back 9 or finished.
+        if (dist.length <= 9){
+            document.getElementById("totalFrontScor").innerHTML = shots.reduce((x, y) => x + y)
+            document.getElementById("totalRndScor").innerHTML = shots.reduce((x, y) => x + y)}
+        else{
+            document.getElementById("totalBkScor").innerHTML = shots.slice(8, shots.length-1).reduce((x, y) => x + y);
+            document.getElementById("totalRndScor").innerHTML = shots.reduce((x, y) => x + y);
+        };
+            highLow();
+            showholength();
 
-    if (dist.length === 18){
-        document.getElementById("holenum").innerHTML = "Round Finished";}
+        if (dist.length === 18){
+            document.getElementById("holenum").innerHTML = "Round Finished";
+            document.getElementById("holeLength").className = "hide";}
+            
         else {
-        document.getElementById("holenum").innerHTML = `Hole ${i+1}`;}
-
-    showholength();
-    score = 0;
-
+            document.getElementById("holenum").innerHTML = `Hole ${i+1}`;
+            showholength();
+            score = 0;}
 };
 
-
+function currentOverUnder(){
+    let i = dist.length;
+    return parseInt(document.getElementById(`ou${i-1}`).innerHTML) + parseInt(shots[i-1] - par[i-1]);
+ }
 
 // Colour code completed hole cells
 function highLow(){
